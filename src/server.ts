fdcef00c91bd0d76a64b540a82e90d2b63e5acf9
kaio-d <dantaskaio01@gmail.com>
2025-08-15 14:08:42 -0300

    express configured

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
-import express, {Request, Response} from 'express'
+import express, {json, Request, Response} from 'express'
 
 const app = express();
+const port = process.env.PORT
+
+app.use(json())
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World')
+    res.status(200).json({player: "Lionel Messi"})
 })
 
-app.listen(3000)
+app.listen(port, () => {
+    console.log(`Servidor running at port: ${port}`);
+})
 
